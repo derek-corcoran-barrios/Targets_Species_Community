060ab53abe0157d29adaf0898c768bcd57ca40a3
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2023-11-29 07:59:32 +0100

    Refined the get_data function and created the first 10 species in order to make a good r question

--- R/functions.R
+++ R/functions.R
@@ -1,9 +1,17 @@
 get_data <- function(file) {
   readxl::read_xlsx(file) |>
     janitor::clean_names() |>
-    dplyr::select(videnskabeligt_navn, taxonrang)
+    dplyr::filter(rige == "Plantae") |>
+    dplyr::filter(taxonrang %in% c("Art", "Form", "Superart", "Underart", "Varietet")) |>
+    dplyr::filter(herkomst != "Introduceret" | is.na(herkomst)) |>
+    dplyr::select(videnskabeligt_navn, taxonrang) |>
+    dplyr::distinct()
 }
 
+read_raster <- function(file) {
+  Result <- terra::wrap(terra::rast(file))
+  return(Result)
+}
 
 clean_species <- function(df){
   Clean_Species <- SDMWorkflows::Clean_Taxa(Taxons = df$videnskabeligt_navn)
@@ -12,7 +20,9 @@ clean_species <- function(df){
 filter_plants <- function(df){
   result <- df |>
     dplyr::filter(kingdom == "Plantae") |>
-    dplyr::pull(species)
+    dplyr::pull(species) |>
+    unique() |>
+    head(10)
   return(result)
 }
 
@@ -26,16 +36,28 @@ get_plant_presences <- function(species){
 }
 
 
-summarise_presences <- function(list){
-  Sum <- list |>
-    purrr::map(~as.data.table(.x[[1]])) |>
-    purrr::map(~.x[, .N, keyby = .(scientificName, family, genus, species)]) |>
-    data.table::rbindlist()
+summarise_presences <- function(df){
+  Sum <- as.data.table(df)[, .N, keyby = .(family, genus, species)]
   return(Sum)
 }
 
 
-#mtcars |> dplyr::group_split(am) |>
-#  purrr::map(~as.data.table(.x)) |>
-#  purrr::map(~.x[, .N, keyby = .(cyl, gear)]) |>
-#
+generate_tree <- function(DF){
+  Tree <- as.data.frame(DF) |>
+    dplyr::select(species, genus, family) |>
+    dplyr::distinct() |>
+    V.PhyloMaker::phylo.maker()
+  return(Tree)
+}
+
+make_buffer_rasterized <- function(DF, raster){
+#  Rast <- terra::unwrap(raster)
+  Result <- DF |>
+    dplyr::select(decimalLatitude, decimalLongitude, family, genus, species) |>
+    terra::vect(geom = c( "decimalLongitude", "decimalLatitude"), crs = "+proj=longlat +datum=WGS84") |>
+    terra::project(terra::crs(Rast)) |>
+    terra::buffer(500) |>
+    terra::rasterize(Rast) |>
+    terra::as.data.frame(cells = T)
+  return(Result)
+}
--- _targets.R
+++ _targets.R
@@ -3,9 +3,14 @@ library(targets)
 source("R/functions.R")
 library(crew)
 
-tar_option_set(packages = c("dplyr", "readxl", "SDMWorkflows", "terra", "janitor", "purrr", "data.table"),
-               controller = crew_controller_local(workers = 12))
+tar_option_set(packages = c("dplyr", "readxl", "SDMWorkflows", "terra", "janitor", "purrr", "data.table", "V.PhyloMaker"),
+               controller = crew_controller_local(workers = 50),
+               error = "null")
 list(
+  tar_target(LandUseTiff,
+             "O:/Nat_Sustain-proj/_user/HanneNicolaisen_au704629/Data/Land_cover_maps/Basemap/basemap_reclass_SN_ModelClass.tif",
+             format = "file"),
+  #tar_target(LanduseRaster, read_raster(LandUseTiff), format = "qs"),
   tar_target(file, "2022-09-21.xlsx", format = "file"),
   tar_target(data, get_data(file)),
   tar_target(Clean, clean_species(data)),
@@ -14,6 +19,9 @@ list(
              get_plant_presences(Only_Plants),
              pattern = map(Only_Plants)),
   tar_target(Presence_summary, summarise_presences(Presences),
-             pattern = head(Presences))
+             pattern = map(Presences)),
+  tar_target(Trees, generate_tree(c))#,
+#  tar_target(buffer_500, make_buffer_rasterized(DF =Presences, Rast = LanduseRaster),
+#             pattern = map(Presences))
 )
 
