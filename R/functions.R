03a08a40a5c220d3fbf8671698bf694fd0790f1f
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2024-02-05 05:02:25 +0100

    3000 species (1004) ready!

--- R/functions.R
+++ R/functions.R
@@ -18,7 +18,7 @@ filter_plants <- function(df){
     dplyr::filter(kingdom == "Plantae") |>
     dplyr::select("family", "genus", "species") |>
     distinct()
-  result <- result[1:2700,]
+  result <- result[1:3000,]
   return(result)
 }
 
